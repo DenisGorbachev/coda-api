0bcde4c6f32d071379fbd11695bac1d39fd6842f
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2025-09-06 15:03:14 +0700

    fix: warning

--- examples/metadata.rs
+++ examples/metadata.rs
@@ -10,7 +10,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let doc_id = args.next().expect("doc_id should be the first argument");
     let client = Client::new_with_key(&key)?;
     let tables = client.tables(&doc_id).await?;
-    let tables_ids = tables.iter().map(|t| t.id.clone());
+    // let tables_ids = tables.iter().map(|t| t.id.clone());
     // let columns_map = client.columns_map(&doc_id, tables_ids).await?;
     let output = json!({
         "tables": tables,
