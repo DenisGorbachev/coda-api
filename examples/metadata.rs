034fc6b76396ec91aa7b5a3c5122ee46512039d0
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2025-09-26 19:52:36 +0700

    fix: use clap

--- examples/metadata.rs
+++ examples/metadata.rs
@@ -1,21 +1,36 @@
+use clap::Parser;
 use coda_api::Client;
 use serde_json::json;
-use std::env::{args, var};
+
+#[derive(Clone, Debug, Parser)]
+pub struct MetadataCli {
+    #[arg(long, short, env = "CODA_API_KEY", hide_env_values = true)]
+    pub api_key: String,
+
+    #[arg(long, short, env = "CODA_DOC_ID")]
+    pub doc_id: String,
+}
+
+impl MetadataCli {
+    pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let client = Client::new_with_key(&self.api_key)?;
+        let tables = client.tables(&self.doc_id).await?;
+        let output = json!({
+            "tables": tables,
+        });
+        println!("{output}");
+        Ok(())
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let key = var("CODA_API_KEY").expect("CODA_API_KEY should be set");
-    let mut args = args();
-    let _bin = args.next();
-    let doc_id = args.next().expect("doc_id should be the first argument");
-    let client = Client::new_with_key(&key)?;
-    let tables = client.tables(&doc_id).await?;
-    // let tables_ids = tables.iter().map(|t| t.id.clone());
-    // let columns_map = client.columns_map(&doc_id, tables_ids).await?;
-    let output = json!({
-        "tables": tables,
-        // "columns_map": columns_map
-    });
-    println!("{output}");
-    Ok(())
+    let cli = MetadataCli::parse();
+    cli.run().await
+}
+
+#[test]
+fn verify_cli() {
+    use clap::CommandFactory;
+    MetadataCli::command().debug_assert();
 }
