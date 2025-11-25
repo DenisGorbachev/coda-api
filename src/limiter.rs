ad68d17b9157819428232cb02ed72a43c8e46077
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2025-11-25 08:51:00 +0700

    fix: rate limits

--- src/limiter.rs
+++ src/limiter.rs
@@ -2,6 +2,7 @@ use governor::{DefaultDirectRateLimiter, Quota};
 use std::num::NonZeroU32;
 use std::time::Duration;
 
+/// The rate limits are slightly lower compared to official docs because I've seen "429 Too Many Requests" errors even with these rate limits
 #[derive(Debug)]
 pub struct Limiter {
     pub read: DefaultDirectRateLimiter,
@@ -11,13 +12,16 @@ pub struct Limiter {
     pub read_analytics: DefaultDirectRateLimiter,
 }
 
+/// Buffer added to official rate limits
+const BUFFER: u64 = 1;
+
 impl Default for Limiter {
     fn default() -> Self {
-        let read = make_burst_limiter(100, Duration::from_secs(6));
-        let write = make_burst_limiter(10, Duration::from_secs(6));
-        let write_doc_content = make_burst_limiter(5, Duration::from_secs(10));
-        let list_docs = make_burst_limiter(4, Duration::from_secs(6));
-        let read_analytics = make_burst_limiter(100, Duration::from_secs(6));
+        let read = make_burst_limiter(100, Duration::from_secs(6 + BUFFER));
+        let write = make_burst_limiter(10, Duration::from_secs(6 + BUFFER));
+        let write_doc_content = make_burst_limiter(5, Duration::from_secs(10 + BUFFER));
+        let list_docs = make_burst_limiter(4, Duration::from_secs(6 + BUFFER));
+        let read_analytics = make_burst_limiter(100, Duration::from_secs(6 + BUFFER));
         Self {
             read,
             write,
